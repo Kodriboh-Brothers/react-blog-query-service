f468dce93f94d274a7ad1ea5199ef881fcc1a699
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-11 21:03:20 +0000

    Add basic post/comment storage

--- server.js
+++ server.js
@@ -10,13 +10,33 @@ app.use(cors());
 
 const port = process.env.QUERY_SERVICE_PORT || 4002;
 
-app.get('/posts', (req, res) => {
+const posts = {}
 
+app.get('/posts', (req, res) => {
+    res.send(posts);
 });
 
 // Receive events from event bus
 app.post('/events', (req, res) => {
+    const { type, data } = req.body;
 
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+
+        posts[id] = {
+            id, title, comments: []
+        };
+        
+    }
+
+    if (type === 'CommentCreated') {
+        const { id, content, postId } = data;
+
+        const post = posts[postId];
+    }
+
+    // Received and processed event
+    res.send({});
 });
 
 app.listen(port, () => {
